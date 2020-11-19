cef773b73cbcc0714be678aa798ca0403fad38f7
MrGorbunov <volarcko@gmail.com>
2020-11-19 18:18:37 -0500

    Created Website Code

---
+++ logic.js
@@ -0,0 +1,72 @@
+var slideVisionPan = document.getElementById("sliderVisionPan");
+var textVisionPan = document.getElementById("spanVisionPan");
+var valVisionPan = 0;
+
+var slideVisionTilt = document.getElementById("sliderVisionTilt");
+var textVisionTilt = document.getElementById("spanVisionTilt");
+var valVisionTilt = 0;
+
+var slideShovel = document.getElementById("sliderShovelServo");
+var textShovel = document.getElementById("spanShovelServo");
+var valShovel = 0;
+
+var slideLeftMotor = document.getElementById("sliderLeftMotor");
+var textLeftMotor = document.getElementById("spanLeftMotor");
+var valLeftMotor = 0;
+
+var slideRightMotor = document.getElementById("sliderRightMotor");
+var textRightMotor = document.getElementById("spanRightMotor");
+var valRightMotor = 0;
+
+
+// This populates the empty spans (starts with text displayed)
+textVisionPan.innerHTML = valVisionPan;
+textVisionTilt.innerHTML = valVisionTilt;
+textShovel.innerHTML = valShovel;
+textLeftMotor.innerHTML = valLeftMotor;
+textRightMotor.innerHTML = valRightMotor;
+
+
+// This code could definitely be rewriten but it works so imm not gonna worry
+slideVisionPan.oninput = function() {
+  textVisionPan.innerHTML = this.value;
+  valVisionPan = this.value;
+  sendData();
+}
+
+slideVisionTilt.oninput = function() {
+  textVisionTilt.innerHTML = this.value;
+  valVisionTilt = this.value;
+  sendData();
+}
+
+slideShovel.oninput = function() {
+  textShovel.innerHTML = this.value;
+  valShovel = this.value;
+  sendData();
+}
+
+slideLeftMotor.oninput = function() {
+  textLeftMotor.innerHTML = this.value;
+  valLeftMotor = this.value;
+  sendData();
+}
+
+slideRightMotor.oninput = function() {
+  textRightMotor.innerHTML = this.value;
+  valRightMotor = this.value;
+  sendData();
+}
+
+
+// The exciting code
+async function sendData () {
+  let valuesString = `leftmotorspeed=${valLeftMotor}&rightmotorspeed=${valRightMotor}` +
+    `&shovelservo=${valShovel}&visionPan=${valVisionPan}&visiontilt=${valVisionTilt}`;
+
+  console.log(valuesString);
+
+  fetch(`http://localhost:8000/bg.html?` + valuesString)
+  .then(response => {} )
+  .catch(error => {} );
+}
